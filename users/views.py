e44f806d540bc3ab04acc4093951f3f4ea6f2020
Marsag2023 <m.aygistov@gmail.com>
2024-06-25 01:14:22 +0300

    Modified files

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from users.models import User
 from users.serializer import UserSerializer
 
@@ -7,3 +7,12 @@ from users.serializer import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        """
+    Любой пользователь может зарегистрироваться на сайте
+        """
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
